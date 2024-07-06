045f2de2f5890b4b37679f25840267491e4c955e
Supriyo-sen <supriyo.webweeb@gmail.com>
2024-07-06 12:20:08 +0530

    1st page done

--- app/(screens)/activity.jsx
+++ app/(screens)/activity.jsx
@@ -1,12 +1,32 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 const activity = () => {
   return (
-    <View >
-      <Text>screen 3 page</Text>
-    </View>
-  );
-};
+    <View style={styles.container}>
+    <ImageBackground source={require("../../assets/images/app-bg.png")} resizeMode="cover" style={styles.image}>
+      <Text style={styles.text}>Inside</Text>
+    </ImageBackground>
+  </View>
+  )
+}
 
-export default activity;
+export default activity
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+  },
+})
